9b21a4fcdf2a64a2d691389ea2fc6038bd95a6f0
zhaominhui <zhaominhui@hzpnc.com>
2015-10-23 11:01:19 +0800

    添加图片加载时的处理

--- js/imageViewer.js
+++ js/imageViewer.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var iv = (function() {
-    var overlay, image, controller, leftRotate, rightRotate, zoomBtn;
-    var margins = 200;
+    var preloadImage, src;
+    var overlay, loading, image, controller, leftRotate, rightRotate, zoomBtn;
+    var margins = 200, wheelDelta = 0.2;
     var ie = /msie/i.test(navigator.userAgent);
 
     /* 工具函数 */
@@ -19,11 +20,11 @@ var iv = (function() {
     } 
 
     function stopPropagation(event) {
-        !ie ? event.stopPropagation() : window.event.cancelBubble = true;
+        !window.event ? event.stopPropagation() : window.event.cancelBubble = true;
     }
 
     function preventDefault(event) {
-        !ie ? event.preventDefault() : window.event.returnValue = false;
+        !window.event ? event.preventDefault() : window.event.returnValue = false;
     }
 
     /* 渲染组件 */
@@ -31,17 +32,31 @@ var iv = (function() {
         addClass(document.body, 'ivActive');
         var overlay = document.createElement('div');
         document.body.appendChild(overlay);
-        overlay.outerHTML = '<div id="ivOverlay"><img id="ivImage" src="' + src + '"/><div id="ivController"></div></div>';
-
+        overlay.outerHTML = '<div id="ivOverlay"><div id="ivLoading"><i class="fa fa-spinner fa-spin fa-3x"></i></div><img id="ivImage" style="display: none" /><div id="ivController"></div></div>';
+        
         controller = document.getElementById('ivController');
         var controllerHtml = '<a id="ivLeftRotate" href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle-thin fa-stack-2x"></i><i class="fa fa-undo fa-stack-1x"></i></span></i></span></a>';
             controllerHtml += '<a id="ivZoom" href="#"><span class="fa-stack fa-2x"><i class="fa fa-circle-thin fa-stack-2x"></i><i class="fa fa-square-o fa-stack-1x"></i></span></i></span></a>';
             controllerHtml += '<a id="ivRightRotate" href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle-thin fa-stack-2x"></i><i class="fa fa-repeat fa-stack-1x"></i></span></i></span></a>';
         controller.innerHTML = controllerHtml;
+
+        loading = document.getElementById('ivLoading');
+        image = document.getElementById('ivImage');
+        zoomBtn = document.getElementById('ivZoom');
+        leftRotate = document.getElementById('ivLeftRotate');
+        rightRotate = document.getElementById('ivRightRotate');
     }
 
     /* 注册事件 */
     function registerEvent() {
+        preloadImage = new Image();
+        preloadImage.onload = function() {
+            loading.style.display = 'none';
+            image.src = src;
+            resetPosition();
+            image.style.display = 'block';
+        }
+
         window.onresize = function() {
             resetPosition();
         }
@@ -56,7 +71,6 @@ var iv = (function() {
             zoomImage(event);
         });
 
-        image = document.getElementById('ivImage');
         image.onclick = function(event) {
             stopPropagation(event);
         }
@@ -88,21 +102,18 @@ var iv = (function() {
             });
         });
 
-        zoomBtn = document.getElementById('ivZoom');
         zoomBtn.onclick = function(event) {
             preventDefault(event);
             stopPropagation(event);
             resetOrZoomIn();
         }
 
-        leftRotate = document.getElementById('ivLeftRotate');
         leftRotate.onclick = function(event) {
             rotateLeft();
             stopPropagation(event);
             preventDefault(event);
         }
 
-        rightRotate = document.getElementById('ivRightRotate');
         rightRotate.onclick = function(event) {
             rotateRight();
             stopPropagation(event);
@@ -174,18 +185,19 @@ var iv = (function() {
     function zoomImage(event) {
         var delta = event.deltaY, scale, position;
         
-        switch (event.deltaMode) {
-            case 0:
-                delta = - delta / 85;
-                break;
-            case 1:
-                delta = - delta / 3;
-                break;
-            case 2:
-            default: 
-                delta = - delta;
-        }
-        scale = 1 + delta / 5;
+        // switch (event.deltaMode) {
+        //     case 0:
+        //         delta = - delta / 85;
+        //         break;
+        //     case 1:
+        //         delta = - delta / 3;
+        //         break;
+        //     case 2:
+        //     default: 
+        //         delta = - delta;
+        // }
+        // scale = 1 + delta / 5;
+        scale = 1 + (delta > 0 ? - wheelDelta : wheelDelta);
         position = getZoomPosition(scale);
 
         return resizeImage(position);
@@ -214,19 +226,22 @@ var iv = (function() {
         var deg = parseInt(image.getAttribute('data-rotate')) || 0;
         deg = clockwise ? (deg + 1) % 4 : (deg + 3) % 4;
         image.setAttribute('data-rotate', deg);
-        if (ie) {
-            image.style.filter = 'progid:DXImageTransform.Microsoft.BasicImage(rotation=' + deg + ');';
-        } else {
-            deg *= 90;
-            image.style.transform = 'rotate(' + deg + 'deg)';
-        }
+        deg *= 90;
+        image.style.msTransform = 'rotate(' + deg + 'deg)';
+        image.style.transform = 'rotate(' + deg + 'deg)';
     }
 
+
     /* 主函数 */
-    function imageViewer(src) {
-        render(src);
+    function imageViewer(url) {
+        src = url;
+        render();
         registerEvent();
-        resetPosition();
+        loadImage();
+    }
+
+    function loadImage() {
+        preloadImage.src = src;
     }
 
     return imageViewer;
